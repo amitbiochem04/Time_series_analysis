35f33b46531a6d432448d089011d553dd16a6b4c
amitbiochem04 <amitbiochem04@gmail.com>
2017-05-24 14:02:20 +0200

    Update sine_wave.R

--- sine_wave.R
+++ sine_wave.R
@@ -1,28 +1,31 @@
 
-data<- log2( norm[,c(29:40)]+1 )
+data<- norm[,c(29:40)] 
 x<-colData(fullData)$time[29:40]
 #x = seq( 0,, by=2)
 approx <- as.data.frame( mat.or.vec( nrow( data), 16))
 pval<- as.vector(c())
 amplitute <- as.vector(c())
 phase <- as.vector(c())
-for( i in 1:nrow( data) ){
-y <- as.numeric( data[ i,1:12 ])
-fit1 <- glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
-fit0 <- glm.nb( y ~ x )
+row_sub = apply(data, 1, function(row) all(row !=0 ))
+data<-data[row_sub,]
+ for( i in 1:nrow( data) ){
+y <-as.numeric( data[ i,1:12 ]+1)
+fit1 <- glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ),na.action=na.omit,control=glm.control(maxit=100) )
+fit0 <- glm.nb( y ~ x ,control=glm.control(maxit=100) )
+	 
 amplitute[i]<- sqrt( coef(fit1)[3]^2 + coef(fit1)[4]^2 )
 #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
-model<-anova( fit0, fit1 )
-p=-1*atan2( coef(fit1)[4], coef( fit1)[3] )*22/( 2*pi )
-  	if (p<0) {
-		p=-1*p
-	} else {
-		p=22-p
-	}
-phase[i]<-p
-pval[i]<-model$`Pr(>F)`[2]
-approx <- as.data.frame( cbind( data,pval,amplitute,phase) )
-approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval))
+model<-anova( fit0, fit1, test="Chisq" )
+#p=-1*atan2( coef(fit1)[4], coef( fit1)[3] )*22/( 2*pi )
+ # 	if (p<0) {
+#		p=-1*p
+#	} else {
+#		p=22-p
+	#}
+#phase[i]<-p
+#pval[i]<-model$`Pr(Chi)`[2]
+#approx <- as.data.frame( cbind( data,pval,amplitute,phase) )
+#approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval))
 print(i)
 }
 ###
