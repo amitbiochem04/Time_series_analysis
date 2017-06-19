45d8890b042df0f897fd08ef9c80c2c4f9373c28
amitbiochem04 <amitbiochem04@gmail.com>
2017-06-19 16:57:01 +0200

    Update sin_wave_lm.R

--- sin_wave_lm.R
+++ sin_wave_lm.R
@@ -81,4 +81,123 @@ y <- calcNormFactors( y)
 v <- voom( y, design, plot=TRUE)
 efit <- eBayes( vfit)
 
+#########final 
+mat <- norm[,c(29:52)]
+#####take avrergae 
+newmat <- as.data.frame( mat.or.vec( nrow( mat), 12))
+newmat[,1] = apply(mat[,c(1,13)],1,mean)
+newmat[,2] = apply(mat[,c(2,14)],1,mean)
+newmat[,3] = apply(mat[,c(3,15)],1,mean)
+newmat[,4] = apply(mat[,c(4,16)],1,mean)
+newmat[,5] = apply(mat[,c(5,17)],1,mean)
+newmat[,6] = apply(mat[,c(6,18)],1,mean)
+newmat[,7] = apply(mat[,c(7,19)],1,mean)
+newmat[,8] = apply(mat[,c(8,20)],1,mean)
+newmat[,9] = apply(mat[,c(9,21)],1,mean)
+newmat[,10] = apply(mat[,c(10,22)],1,mean)
+newmat[,11] = apply(mat[,c(11,23)],1,mean)
+newmat[,12] = apply(mat[,c(12,24)],1,mean)
+rownames(newmat)<-rownames(mat)
+colnames(newmat)<-c("0hr","2hr","4hr","6hr","8hr","10hr","12hr","14hr","16hr","18hr","20hr","22hr")
+save(newmat,file="dark_rep_mean.rda")
+newmat<-log2(newmat[,c(2:12)]+1)
+
+x<-colData(fullData)$time[c(30:40)]
+#data<-newmat[,c(1:12)]
+##
+approx <- as.data.frame( mat.or.vec( nrow( newmat), 15))
+pval<- as.vector( c() )
+amplitute <- as.vector( c() )
+phase <- as.vector( c() )
+ 
+ for( i in 1:nrow( newmat ))  {
+  y <- as.numeric( newmat[i,1:11 ] )
+  fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  fit0 <- lm( y ~ x )
+  #fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  #fit0 <-glm.nb( y ~ x )
+  amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
+  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+  phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+  model<-anova( fit0, fit1 )
+  #model<-anova( fit0, fit1, test="Chisq" )
+  #pval[i]<-model$`Pr(>F)`[2]
+  pval[i]<-model$`Pr(>F)`[2]
+  #pval[i]<-model[["Pr(Chi)"]][2]
+
+  print(i)
+ }
+
+approx <- as.data.frame( cbind(newmat, pval, amplitute, phase ) )
+#approx$p.adj<-p.adjust( pval, method = "BH",n = length( pval ) )
+approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval ) )
+#approx$p.adj_homel<-p.adjust( approx$pval, method = "hommel",n = length( approx$pval ) )
+#approx$p.adj_homel<-p.adjust( approx$pval, method = "bonferroni",n = length( approx$pval ) )
+
+#######
+#sig<- subset(approx,p.adj< 0.1)
+sig<-approx[ order(approx[,"p.adj"] ), ]
+keep<-list()
+for (i in rownames( sig)){
+  idx<-grep(i,anno$X.Gene.ID.)
+  idx<-anno[ idx,]
+  keep[[i]]<-idx
+} 
+df<-melt(keep,id=c("X.Gene.ID.","X.Gene.Name.or.Symbol."))
+df<-df[,1:2]
+colnames(df)<-c("ens.id","symbol")
+df<-df[!duplicated(df), ]
+sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
+save(sig,file="ryth_dd_rep_with_out_0_log.rda")
+
+
+#######
+index=(apply(newmat,1,sum)>20) 
+
+row_sub = apply(newmat, 1, function(row) all(row !=0 ))
+data<-newmat[index,]
+
+approx <- as.data.frame( mat.or.vec( nrow(data), 15))
+pval<- as.vector( c() )
+amplitute <- as.vector( c() )
+phase <- as.vector( c() )
+x<-colData(fullData)$time[c(29:40)]
+for( i in 1:nrow( data ))  {
+  y <- as.numeric( data[i,1:12 ] )
+  #fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  #fit0 <- lm( y ~ x )
+  fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  fit0 <-glm.nb( y ~ x )
+  amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
+  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+  phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+  model<-anova( fit0, fit1 )
+  model<-anova( fit0, fit1, test="Chisq" )
+  #pval[i]<-model$`Pr(>F)`[2]
+  pval[i]<-model$`Pr(>F)`[2]
+  #pval[i]<-model[["Pr(Chi)"]][2]
+#  approx <- as.data.frame( cbind( data, pval, amplitute, phase ) )
+ # approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval ) )
+  #approx$p.adj_by<-p.adjust( approx$pval, method = "BY",n = length( approx$pval ) )
+  #approx$p.adj_homel<-p.adjust( approx$pval, method = "hommel",n = length( approx$pval ) )
+  #approx$p.adj_homel<-p.adjust( approx$pval, method = "bonferroni",n = length( approx$pval ) )
+  print(i)
+}
+
+#######
+sig<- subset(approx,p.adj< 0.1)
+sig<-approx[ order(approx[,"p.adj"] ), ]
+keep<-list()
+for (i in rownames( sig)){
+  idx<-grep(i,anno$X.Gene.ID.)
+  idx<-anno[ idx,]
+  keep[[i]]<-idx
+} 
+df<-melt(keep,id=c("X.Gene.ID.","X.Gene.Name.or.Symbol."))
+df<-df[,1:2]
+colnames(df)<-c("ens.id","symbol")
+#df<-df[!duplicated(df), ]
+sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
+save(sig,file="ryth_dd_rep_with__out_0_log.rda")
+
 
