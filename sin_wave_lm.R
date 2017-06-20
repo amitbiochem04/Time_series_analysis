86f64bdada3b487127889b38bef02d35545ca0bc
amitbiochem04 <amitbiochem04@gmail.com>
2017-06-20 14:39:35 +0200

    Update sin_wave_lm.R

--- sin_wave_lm.R
+++ sin_wave_lm.R
@@ -36,6 +36,35 @@ sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
 
 
 
+####plot
+dd<-subset(sig,p.adj < 0.1)
+dd<-dd[ order( dd[,"phase"] ), ]
+#dd$phase<-abs(dd$phase)
+mydata<-dd[,c(1:11)]
+category=c(rep("Day_gene", 50),rep("Morning_and_evening_gene", 14), rep("Morning_gene",16 ))
+mydf<-data.frame(row.names(mydata),category)
+CairoPDF(file="Rythmic_gene_with 0h p.adj < 0.1",width=10/1.54,height=10/1.54)
+#heatmap.2(as.matrix( dd[,c(1:11)]),Colv=F,col=greenred(30),trace="none",scale="row",dendrogram = "none")
+heatmap.2(as.matrix(mydata), col=greenred(30), trace="none",
+          Colv=FALSE, Rowv=FALSE, dendrogram = "none",scale="row",rowsep = c(50,64),
+          keysize=0.5,labRow=NA,density.info="none",
+        RowSideColors=col1[as.numeric(mydf$category)] )
+#legend(x="center", legend=c("genes","genes2","Morning"),col=col1[as.numeric(mydf$category)], pch=15)
+#legend(x="topleft", legend=c("genes","genes2","Morning"), col=c("red","blue","black"), pch=15)
+
+#legend("topright",      
+ #      legend = category,
+  #     col = col1[as.numeric(mydf$category)], lty= 1,             
+   #    lwd = 5,cex=.7)
+
+
+
+
+
+
+
+
+
 
 
 ####
@@ -100,7 +129,7 @@ newmat[,12] = apply(mat[,c(12,24)],1,mean)
 rownames(newmat)<-rownames(mat)
 colnames(newmat)<-c("0hr","2hr","4hr","6hr","8hr","10hr","12hr","14hr","16hr","18hr","20hr","22hr")
 save(newmat,file="dark_rep_mean.rda")
-newmat<-log2(newmat[,c(2:12)]+1)
+newmat<-log2(newmat[,c(1:12)]+1)
 
 x<-colData(fullData)$time[c(30:40)]
 #data<-newmat[,c(1:12)]
@@ -111,14 +140,14 @@ amplitute <- as.vector( c() )
 phase <- as.vector( c() )
  
  for( i in 1:nrow( newmat ))  {
-  y <- as.numeric( newmat[i,1:11 ] )
+  y <- as.numeric( newmat[i,2:11 ] )
   fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
   fit0 <- lm( y ~ x )
   #fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
   #fit0 <-glm.nb( y ~ x )
   amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
   #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
-  phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+  phase[i]=-atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
   model<-anova( fit0, fit1 )
   #model<-anova( fit0, fit1, test="Chisq" )
   #pval[i]<-model$`Pr(>F)`[2]
@@ -149,6 +178,90 @@ colnames(df)<-c("ens.id","symbol")
 df<-df[!duplicated(df), ]
 sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
 save(sig,file="ryth_dd_rep_with_out_0_log.rda")
+####
+dd<-subset(sig,p.adj < 0.1)
+dd<-dd[ order( dd[,"phase"] ), ]
+dd$phase<-abs(dd$phase)
+mydata<-dd[,c(1:11)]
+category=c(rep("Day_gene", 50),rep("Morning_and_evening_gene", 14), rep("Morning_gene",16 ))
+mydf<-data.frame(row.names(mydata),category)
+col1<-c("red","blue","black")
+CairoPDF(file="Rythmic_gene_with out 0h",width=10/1.54,height=10/1.54)
+#heatmap.2(as.matrix( dd[,c(1:11)]),Colv=F,col=greenred(30),trace="none",scale="row",dendrogram = "none")
+heatmap.2(as.matrix(mydata), col=greenred(30), trace="none",
+          Colv=FALSE, Rowv=FALSE, dendrogram = "none",scale="row",rowsep = c(50,64),
+         labRow=NA,density.info="none",
+        RowSideColors=col1[as.numeric(mydf$category)] )
+dev.off()
+
+######
+
+
+x<-colData(fullData)$time[c(29:40)]
+#data<-newmat[,c(1:12)]
+##
+approx <- as.data.frame( mat.or.vec( nrow( newmat), 15))
+pval<- as.vector( c() )
+amplitute <- as.vector( c() )
+phase <- as.vector( c() )
+ 
+ for( i in 1:nrow( newmat ))  {
+  y <- as.numeric( newmat[i,1:12 ] )
+  fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  fit0 <- lm( y ~ x )
+  #fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+  #fit0 <-glm.nb( y ~ x )
+  amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
+  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+  phase[i]=-atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+  model<-anova( fit0, fit1 )
+  #model<-anova( fit0, fit1, test="Chisq" )
+  #pval[i]<-model$`Pr(>F)`[2]
+  pval[i]<-model$`Pr(>F)`[2]
+  #pval[i]<-model[["Pr(Chi)"]][2]
+
+  print(i)
+ }
+
+approx <- as.data.frame( cbind(newmat, pval, amplitute, phase ) )
+#approx$p.adj<-p.adjust( pval, method = "BH",n = length( pval ) )
+approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval ) )
+#approx$p.adj_homel<-p.adjust( approx$pval, method = "hommel",n = length( approx$pval ) )
+#approx$p.adj_homel<-p.adjust( approx$pval, method = "bonferroni",n = length( approx$pval ) )
+
+#######
+#sig<- subset(approx,p.adj< 0.1)
+sig<-approx[ order(approx[,"p.adj"] ), ]
+keep<-list()
+for (i in rownames( sig)){
+  idx<-grep(i,anno$X.Gene.ID.)
+  idx<-anno[ idx,]
+  keep[[i]]<-idx
+} 
+df<-melt(keep,id=c("X.Gene.ID.","X.Gene.Name.or.Symbol."))
+df<-df[,1:2]
+colnames(df)<-c("ens.id","symbol")
+df<-df[!duplicated(df), ]
+sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
+write.xlsx(sig, "rythmicgene.xlsx",row.names=TRUE)
+#####plot
+dd<-subset(sig,p.adj < 0.1)
+dd<-dd[ order( dd[,"phase"] ), ]
+dd$phase<-abs(dd$phase)
+mydata<-dd[,c(1:12)]
+category=c(rep("Day_gene", 50),rep("Morning_and_evening_gene", 14), rep("Morning_gene",16 ))
+mydf<-data.frame(row.names(mydata),category)
+col1<-c("red","blue","black")
+CairoPDF(file="Rythmic_gene_with out 0h",width=10/1.54,height=10/1.54)
+#heatmap.2(as.matrix( dd[,c(1:11)]),Colv=F,col=greenred(30),trace="none",scale="row",dendrogram = "none")
+heatmap.2(as.matrix(mydata), col=greenred(30), trace="none",
+          Colv=FALSE, Rowv=FALSE, dendrogram = "none",scale="row",rowsep = c(50,64),
+         labRow=NA,density.info="none",
+        RowSideColors=col1[as.numeric(mydf$category)] )
+dev.off()
+
+###########
+
 
 
 #######
@@ -169,7 +282,7 @@ for( i in 1:nrow( data ))  {
   fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
   fit0 <-glm.nb( y ~ x )
   amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
-  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+  #phase[i]<- -atan2( coef(fit1)[4], coef( fit1)[3] )
   phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
   model<-anova( fit0, fit1 )
   model<-anova( fit0, fit1, test="Chisq" )
